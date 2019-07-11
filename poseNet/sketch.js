4eb10598bae497ef198982957c3886d7bcae8dfa
Yining Shi <ys2189@nyu.edu>
2019-07-11 12:21:08 +0200

    adding flowers on nose

--- poseNet/sketch.js
+++ poseNet/sketch.js
@@ -1,19 +1,29 @@
 let video;
 let myPoseNet;
 let noseX, noseY;
+let headX, headY;
 
 function setup() {
   video = createCapture(VIDEO);
   video.hide();
   createCanvas(600, 400);
   myPoseNet = ml5.poseNet(video, modelReady);
-  fill(255, 0, 0);
   noStroke();
 }
 
 function draw() {
   image(video, 0, 0);
-  ellipse(noseX, noseY, 50, 50);
+  fill(0, 0, 255);
+  rect(headX - 100, headY - 25, 200, 100);
+  // ellipse(noseX, noseY, 50, 50);
+  // rect(noseX - 25, noseY - 25, 50, 50);
+  // A design for a simple flower
+  fill(255, 0, 0);
+  translate(noseX, noseY);
+  for (let i = 0; i < 6; i ++) {
+    ellipse(0, 30, 30, 60);
+    rotate(PI/3);
+  }
 }
 
 function modelReady() {
@@ -23,6 +33,8 @@ function modelReady() {
     if (results && results[0] && results[0].pose && results[0].pose.nose) {
       noseX = results[0].pose.nose.x;
       noseY = results[0].pose.nose.y;
+      headX = noseX;
+      headY = noseY - 200;
     }
   })
 }
